43600a4fa51a14b71168a38d583799cbcff497d1
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-02-12 21:38:41 +0530

    Synchronize Navigation AAC, app bar and bottom bar

--- app/src/main/java/com/bitecode/autobite/screens/shared/view/MainActivity.kt
+++ app/src/main/java/com/bitecode/autobite/screens/shared/view/MainActivity.kt
@@ -2,12 +2,24 @@ package com.bitecode.autobite.screens.shared.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.bitecode.autobite.R
+import kotlinx.android.synthetic.main.act_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_main)
+
+        bottomNavBNV.setupWithNavController(findNavController(R.id.navHostFRA))
+
+        findNavController(R.id.navHostFRA).addOnDestinationChangedListener { _, destination, _ ->
+            screenTitleLBL.text = when(destination.id) {
+                R.id.history -> "History"
+                else         -> throw IllegalStateException("Navigated to destination: ${destination.label}")
+            }
+        }
     }
 }

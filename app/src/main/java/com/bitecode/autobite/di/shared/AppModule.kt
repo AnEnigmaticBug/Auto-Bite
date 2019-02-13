2535720c1d84efc0167b16d38b3e14904dc34399
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-02-13 20:46:38 +0530

    Make SharedPreferences and UserRepository be provided by di

--- app/src/main/java/com/bitecode/autobite/di/shared/AppModule.kt
+++ app/src/main/java/com/bitecode/autobite/di/shared/AppModule.kt
@@ -1,9 +1,13 @@
 package com.bitecode.autobite.di.shared
 
 import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.room.Room
 import com.bitecode.autobite.screens.shared.data.repo.TripRepository
 import com.bitecode.autobite.screens.shared.data.repo.TripRepositoryImpl
+import com.bitecode.autobite.screens.shared.data.repo.UserRepository
+import com.bitecode.autobite.screens.shared.data.repo.UserRepositoryImpl
 import com.bitecode.autobite.screens.shared.data.room.AppDatabase
 import com.bitecode.autobite.screens.shared.data.room.TripsDao
 import dagger.Module
@@ -15,11 +19,21 @@ class AppModule(private val application: Application) {
 
     @Singleton
     @Provides
+    fun providesUserRepository(sharedPreferences: SharedPreferences): UserRepository =
+        UserRepositoryImpl(sharedPreferences)
+
+    @Singleton
+    @Provides
     fun providesTripRepository(tripsDao: TripsDao): TripRepository = TripRepositoryImpl(tripsDao)
 
     @Singleton
     @Provides
     fun providesTripsDao(appDatabase: AppDatabase): TripsDao = appDatabase.tripsDao()
+
+    @Singleton
+    @Provides
+    fun providesSharedPreferences(application: Application): SharedPreferences =
+        application.getSharedPreferences("autobite.sp", Context.MODE_PRIVATE)
 
     @Singleton
     @Provides

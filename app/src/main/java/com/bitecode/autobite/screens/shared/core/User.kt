b114e182698c79dfbb3d6c3dd978998570ef202e
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-02-13 20:43:02 +0530

    Make User core data class

---
+++ app/src/main/java/com/bitecode/autobite/screens/shared/core/User.kt
@@ -0,0 +1,6 @@
+package com.bitecode.autobite.screens.shared.core
+
+/**
+ * @property balance  is the amount(in INR) present in the wallet.
+ * */
+data class User(val id: Long, val name: String, val balance: Long)

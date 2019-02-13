e80729ebe82d80a5943d71855fdc163d4cca037e
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-02-13 11:22:37 +0530

    Integrate HistoryFragment with it's ViewModel

--- app/src/main/java/com/bitecode/autobite/screens/history/view/HistoryFragment.kt
+++ app/src/main/java/com/bitecode/autobite/screens/history/view/HistoryFragment.kt
@@ -4,15 +4,51 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.bitecode.autobite.R
+import com.bitecode.autobite.screens.history.core.HistoryViewModel
+import com.bitecode.autobite.screens.history.core.HistoryViewModelFactory
+import com.bitecode.autobite.screens.history.core.UiOrder
+import com.bitecode.autobite.screens.shared.core.Trip
+import kotlinx.android.synthetic.main.fra_history.view.tripsRCY
 
-class HistoryFragment : Fragment() {
+class HistoryFragment : Fragment(), TripsAdapter.ClickListener {
 
-    private lateinit var rootPOV: View
+    private val viewModel by lazy {
+        ViewModelProviders.of(this, HistoryViewModelFactory())[HistoryViewModel::class.java]
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        rootPOV = inflater.inflate(R.layout.fra_history, container, false)
+        val rootPOV = inflater.inflate(R.layout.fra_history, container, false)
+
+        rootPOV.tripsRCY.adapter = TripsAdapter(this)
+
+        viewModel.orderData.observe(this, Observer { order ->
+            when(order) {
+                is UiOrder.ShowWorking -> showWorkingState(order.trips)
+                is UiOrder.ShowFailure -> showFailureState(order.error)
+            }
+        })
+
         return rootPOV
+    }
+
+
+    override fun onTripClicked(id: Long) {
+        // Code for showing the trip details dialog
+    }
+
+
+    private fun showWorkingState(trips: List<Trip>) {
+        view?.let { view ->
+            (view.tripsRCY.adapter as TripsAdapter).trips = trips
+        }
+    }
+
+    private fun showFailureState(error: String) {
+        Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
     }
 }
